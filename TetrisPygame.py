36eb8e075985413638b3829e410abd63216ab719
Fuat <robo.ai@outlook.com>
2023-02-25 20:23:47 +0000

    usual update

--- TetrisPygame.py
+++ TetrisPygame.py
@@ -27,22 +27,25 @@ def draw_grid(box_top):
                          (start_x + (x * grid_spacing), end_y), 1)
 
 
-
 class Tetrimino:
     def __init__(self):
-        self.shape: float = [0,0]
+        self.pos: float = [0,0]
+        self.shape: float = [[0,0]]
 
-    def move_me(self, x:float, y:float):
-        self.shape[0] += x
-        self.shape[1] += y
+    def set_pos(self, x: float, y: float):
+        self.pos[0] = x
+        self.pos[1] = y
+
+    def add_to_pos(self, x:float, y:float):
+        self.pos[0] += x
+        self.pos[1] += y
 
     def get_bounds(self):
-        return [self.shape[0]-1, self.shape[1]-1, 1, 1]
+        return [self.pos[0], self.pos[1]]
 
-    def check_collision(self, dot):
-        others_bounds = dot.get_bounds()
-        if (others_bounds[0] <= self.shape[0] and self.shape[0] <= (others_bounds[0] + others_bounds[2]) and 
-            others_bounds[1] <= self.shape[1] and self.shape[1] <= (others_bounds[1] + others_bounds[3])):
+    def check_collision(self, tetrimino) -> bool:
+        others_bounds = tetrimino.get_bounds()
+        if(self.pos[0] == others_bounds[0] and self.pos[1] == others_bounds[1]):
             return True
         else:
             return False
@@ -51,7 +54,7 @@ class Tetrimino:
     def rotate(degrees: int):
         pass
 
-    # inherited object can draw itself
+    # object can draw itself
     def draw(self, pygame, screen):
         pass
 
@@ -60,7 +63,27 @@ class Tetrimino:
         pass
 
 
-class TetriminoBlock(Tetrimino):
+class TetriminoDot(Tetrimino):
+    def __init__(self):
+        self.shape: float = [0,0]
+
+    def add_to_pos(self, x:float, y:float):
+        self.shape[0] += x
+        self.shape[1] += y
+
+    def get_bounds(self):
+        return [self.shape[0]-1, self.shape[1]-1, 1, 1]
+
+    def check_collision(self, dot) -> bool:
+        others_bounds = dot.get_bounds()
+        if (others_bounds[0] <= self.shape[0] and self.shape[0] <= (others_bounds[0] + others_bounds[2]) and 
+            others_bounds[1] <= self.shape[1] and self.shape[1] <= (others_bounds[1] + others_bounds[3])):
+            return True
+        else:
+            return False
+
+
+class TetriminoBlock(TetriminoDot):
     def __init__(self):
         self.shape = [[]]
         self.origin: float = [0,0]
@@ -72,22 +95,23 @@ class TetriminoBlock(Tetrimino):
             self.shape[i][1] = (self.shape[i][1] * grid_square_size) + y
 
     # add x and y to their respective coordinates
-    def move_me(self, x: float, y: float):
+    def add_to_pos(self, x: float, y: float):
         for coords in self.shape:
             coords[0] += x
             coords[1] += y
 
     def rotate(self, degrees: int):
+        abc = grid_rect[0] + (grid_square_size / 2) + 1
         for coords in self.shape:
-            new_x = (coords[0] * math.cos(degrees)) - (coords[1] * math.sin(degrees))
-            new_y = (coords[1] * math.cos(degrees)) - (coords[0] * math.sin(degrees))
+            new_x = (((coords[0] / grid_square_size) - abc) * math.cos(degrees)) - (((coords[1] / grid_square_size) - abc) * math.sin(degrees))
+            new_y = (((coords[1] / grid_square_size) - abc) * math.cos(degrees)) + (((coords[0] / grid_square_size) - abc) * math.sin(degrees))
             coords[0] = new_x
             coords[1] = new_y
 
 
 class IBlock(TetriminoBlock):
     def __init__(self):
-        self.shape = [[0,0],[1,0],[2,0],[3,0]]
+        self.shape = [[0,0],[1,0],[2,0],[3,0],[4,0]]
     
 
 class SingleSquare:
@@ -145,16 +169,17 @@ single_box.rect = [grid_rect[0], 0, grid_square_size, grid_square_size]
 single_dot = Tetrimino()
 single_dot.shape = [grid_rect[0] + (grid_square_size / 2) + 1,
                  grid_rect[1] + (grid_square_size / 2) + 1]
-single_dot.move_me(grid_square_size * 5, grid_square_size * 5)
+single_dot.add_to_pos(grid_square_size * 5, grid_square_size * 5)
 #------------------
 moving_dot = Tetrimino()
 moving_dot.shape = [grid_rect[0] + (grid_square_size / 2) + 1,
                  grid_rect[1] + (grid_square_size / 2) + 1]
-moving_dot.move_me(grid_square_size, grid_square_size)
+moving_dot.add_to_pos(grid_square_size, grid_square_size)
 #------------------
 i_block = IBlock()
 i_block.offset_coords(grid_rect[0] + (grid_square_size / 2) + 1,
                       grid_rect[1] + (grid_square_size / 2) + 1)
+#i_block.rotate(90)
 
 
 while running:
@@ -182,7 +207,7 @@ while running:
         #pygame.display.set_caption(str(grid_square_size))
 
         # do a dummy move to check for collision
-        i_block.move_me(grid_square_size, grid_square_size)
+        i_block.add_to_pos(grid_square_size, grid_square_size)
 
         # if collided then..
         #if( i_block.check_collision(single_dot) ):
